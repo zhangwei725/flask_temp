cb1baeedfddbdef8a24b48cc2258c0dd662c23da
zhangwei <18614068889@163.com>
2018-09-29 15:38:24 +0800

    ajax图片上传

--- apps/upload/views.py
+++ apps/upload/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify
 
 from apps.ext import images_upload
 from apps.upload.helper import get_new_file_name
@@ -17,3 +17,19 @@ def upload_img():
         name = images_upload.save(file_store, name=get_new_file_name(file_store.filename))
         url = images_upload.url(name)
         return url
+
+
+@upload.route('/upload/ajax/', methods=['post'])
+def upload_ajax():
+    username = request.values.get('username')
+    password = request.values.get('password')
+    file_store = request.files.get('img')
+    name = images_upload.save(file_store, name=get_new_file_name(file_store.filename))
+    # 返回图片访问的地址
+    url = images_upload.url(name)
+    result = {
+        'status': 200,
+        'msg': 'upload success',
+        'url': url
+    }
+    return jsonify(result)
